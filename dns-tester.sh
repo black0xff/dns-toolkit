c2547aa3804d71476ab73c2a8ca70e768c75cf85
black 01 <70566067+black0xff@users.noreply.github.com>
2025-01-07 20:38:11 +0300

    Update dns-tester.sh

--- dns-tester.sh
+++ dns-tester.sh
@@ -2,30 +2,24 @@
 
 servers='google.com facebook.com youtube.com yahoo.com baidu.com wikipedia.org amazon.com twitter.com taobao.com qq.com google.co.in live.com linkedin.com sina.com.cn weibo.com yahoo.co.jp tmall.com yandex.ru blogspot.com ebay.com google.co.jp google.de hao123.com vk.com instagram.com bing.com reddit.com google.co.uk sohu.com pinterest.com tumblr.com amazon.co.jp wordpress.com google.com.br msn.com google.fr imgur.com paypal.com microsoft.com aliexpress.com apple.com alibaba.com'
 
+
 red="\e[31m"
 green="\e[32m"
-
-# Check if bc is installed
-if ! command -v bc &> /dev/null; then
-  # Install bc using apt
-  sudo apt install bc
-fi
-
 for server in $servers; do
-    start_time=$(date +%S)
+    start_time=$(date +%s%3N)
     dig $server >/dev/null 2>&1
-    end_time=$(date +%S)
+    end_time=$(date +%s%3N)
     elapsed_time=$(echo "scale=3; $end_time - $start_time" | bc)
     echo "$server: take $elapsed_time seconds to resolve "
-    if [[ $elapsed_time < 2 ]];then
-        weak=1
-    else
+    if [[ $elapsed_time > 50 ]];then
         weak=0
+    else
+        true
     fi
 done
 
-if [[ $weak == 1 ]];then
-    echo -e $green'powerfull dns'
-else
+if [[ $weak == 0 ]];then
     echo -e $red'weak dns , need to change now !'
+else
+    echo -e $green'powerfull dns'
 fi
